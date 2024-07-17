90b8b933186a31df94061edd29bb6ee38b3f0d7d
felipe132005 <soporte@lempsoft.com>
2024-07-17 16:07:58 -0500

    implementacion de mainlayout en la pagina Inicio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import TopBar from "../components/Navbar/TopBar"
-
+import MainLayout from "../layouts/MainLayout"
 const Home = () => {
     return (
     <>
-      <TopBar></TopBar>
+      <MainLayout>
+        <main>
+          <h1 className="text-2xl font-semibold">Home</h1>
+        </main>
+      </MainLayout>
     </>
   )
 }
