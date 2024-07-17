41f33b09ac426fb67c65a9bedeb532f7990adc20
felipe132005 <soporte@lempsoft.com>
2024-07-17 16:14:13 -0500

    centrar los elementos del container layout

---
+++ src/layouts/ContainerLayout.tsx
@@ -0,0 +1,14 @@
+interface ContainerLayoutProps {
+    children: React.ReactNode;
+}
+const ContainerLayout = ({children}:ContainerLayoutProps) => {
+  return (
+    <div className="w-full flex flex-wrap items-center justify-center">
+        <div className="w-3/4">
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default ContainerLayout
