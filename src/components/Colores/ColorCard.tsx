4f07da044852a24522811c14d1164288ce6ed9a1
felipe132005 <soporte@lempsoft.com>
2024-07-22 09:03:15 -0500

    Creacion de componente ColorCard e implemetacion de ruta /recursos/colores

---
+++ src/components/Colores/ColorCard.tsx
@@ -0,0 +1,25 @@
+interface ColorCardProps {
+    title: string
+    description: string
+    hexa: string
+}
+import { useState } from "react"
+
+const ColorCard = ({ description, hexa, title }: ColorCardProps) => {
+    const [bgcolor, setBgcolor] = useState(hexa)
+    return (
+        <div className="flex flex-row rounded-lg overflow-hidden shadow-lg">
+            <div className="w-1/2 h-full cursor-pointer"
+                style={{ backgroundColor: bgcolor }}
+                onClick={() => setBgcolor(hexa)}></div>
+            <div className="w-1/2 p-4">
+                <h4 className="font-medium">{title}</h4>
+                <p className="text-sm text-gray-500">{description}</p>
+                <input type="color" value={bgcolor} onChange={(e) => setBgcolor(e.target.value)} />
+                <p className="bg-slate-200 w-auto p-1 rounded-md cursor-pointer" onClick={() => navigator.clipboard.writeText(bgcolor)}><span className="font-medium">Hexa: </span><span>{bgcolor}</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default ColorCard
--- src/pages/Recursos/Colores.tsx
+++ src/pages/Recursos/Colores.tsx
@@ -1,13 +1,19 @@
 import MainLayout from "../../layouts/MainLayout"
 import ContainerLayout from "../../layouts/ContainerLayout"
+import ColorCard from "../../components/Colores/ColorCard"
 const Colores = () => {
   return (
     <MainLayout>
-        <main>
-            <ContainerLayout>
-                <h1>Colores</h1>
-            </ContainerLayout>
-        </main>
+      <main>
+        <ContainerLayout>
+          <h1 className="text-2xl font-semibold">Colores Empresariales</h1>
+          <section className="grid grid-cols-4 gap-2 mt-8">
+            <ColorCard title="Verde Principal" description="Color principal de la marca" hexa="#008594" />
+            <ColorCard title="Negro principal" description="Negro principal de la marca" hexa="#242d3d" />
+            <ColorCard title="Rojo principal" description="Rojo principal de la marca" hexa="#f03f37" />
+          </section>
+        </ContainerLayout>
+      </main>
     </MainLayout>
   )
 }
