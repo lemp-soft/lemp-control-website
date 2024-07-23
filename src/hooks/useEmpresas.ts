b20204be25f5f6929926eb4eac4fe54d77a7a7eb
felipe132005 <soporte@lempsoft.com>
2024-07-23 12:11:49 -0500

    añadir el toque a useEmpresas

--- src/hooks/useEmpresas.ts
+++ src/hooks/useEmpresas.ts
@@ -3,7 +3,11 @@ export function useEmpresas() {
     const { data, isLoading, isError } = useQuery({
         queryKey: ['empresas'],
         queryFn: async () => {
-            const response = await fetch('http://127.0.0.1:8000/api/v1/empresas')
+            const response = await fetch('http://127.0.0.1:8000/api/v1/empresas',{
+                headers: {
+                    Authorization: localStorage.getItem('token') ?? ''
+                }
+            })
             if (!response.ok) {
                 throw new Error('Network response was not ok')
             }
