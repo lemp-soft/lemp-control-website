6ba5aa2e1162cc3289f037409334348fc257ddb1
felipe132005 <soporte@lempsoft.com>
2024-07-23 11:13:01 -0500

    creacion de link a registrarse en el TopBar

--- src/components/Navbar/TopBar.tsx
+++ src/components/Navbar/TopBar.tsx
@@ -47,6 +47,9 @@ const TopBar = () => {
                                 </ul>
                             </div>
                         </li>
+                        <li>
+                            <Link to="/registrarse" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Registrarse</Link>
+                        </li>
                     </ul>
                 </div>
             </div>
