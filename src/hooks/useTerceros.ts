1237fbe9d637c9a7703ad0654c8622b53aa666d8
felipe132005 <soporte@lempsoft.com>
2024-07-23 12:11:58 -0500

    añadir el toque a useTerceros

--- src/hooks/useTerceros.ts
+++ src/hooks/useTerceros.ts
@@ -10,7 +10,11 @@ export function useTerceros() {
     const { data, isLoading, isError } = useQuery({
         queryKey: ['terceros'],
         queryFn: async () => {
-            const response = await fetch('http://127.0.0.1:8000/api/v1/terceros')
+            const response = await fetch('http://127.0.0.1:8000/api/v1/terceros',{
+                headers: {
+                    Authorization: localStorage.getItem('token') ?? ''
+                }
+            })
             return response.json() as Promise<fechedTerceros>
         }
     })
