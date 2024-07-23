b43f24d0f034b2c8d25ae3d9626373e22ed3b48f
felipe132005 <soporte@lempsoft.com>
2024-07-23 11:17:00 -0500

    cambiar link registrarse por iniciar

--- src/components/Navbar/TopBar.tsx
+++ src/components/Navbar/TopBar.tsx
@@ -48,7 +48,7 @@ const TopBar = () => {
                             </div>
                         </li>
                         <li>
-                            <Link to="/registrarse" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Registrarse</Link>
+                            <Link to="/iniciar" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">iniciar</Link>
                         </li>
                     </ul>
                 </div>
