572e0834113d3fc7f905ec93c1765ce31f893517
felipe132005 <soporte@lempsoft.com>
2024-07-24 15:27:52 -0500

    creacion de Link a /administracion/terceros

--- src/layouts/ContainerLayout.tsx
+++ src/layouts/ContainerLayout.tsx
@@ -1,12 +1,12 @@
 interface ContainerLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
-const ContainerLayout = ({children}:ContainerLayoutProps) => {
+const ContainerLayout = ({ children }: ContainerLayoutProps) => {
   return (
-    <div className="w-full flex flex-wrap items-center justify-center">
-        <div className="w-3/4">
-            {children}
-        </div>
+    <div className={`w-full flex flex-wrap items-center justify-center`}>
+      <div className="w-3/4">
+        {children}
+      </div>
     </div>
   )
 }
--- src/pages/Administracion/index.tsx
+++ src/pages/Administracion/index.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom"
+import ContainerLayout from "../../layouts/ContainerLayout"
+import MainLayout from "../../layouts/MainLayout"
+
 const AdministracionPage = () => {
   return (
-    <div>administracionPage</div>
+    <MainLayout>
+      <main>
+        <ContainerLayout>
+          <nav className="grid grid-cols-4 gap-4 mt-6">
+            <Link to={'/administracion/terceros'} className="flex hover:shadow-2xl hover:shadow-slate-950 items-center p-2">
+              <img src="/imgs/Letra_TN.png" alt="letra T" className="w-12 ml-2 aspect-square" />
+              <span className="text-lg font-semibold">Terceros</span>
+            </Link>
+          </nav>
+        </ContainerLayout>
+      </main>
+    </MainLayout>
   )
 }
 
