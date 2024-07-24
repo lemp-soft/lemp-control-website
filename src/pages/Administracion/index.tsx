4490f8b6c64a8e7d9f36e6bf7a7643c22530a505
felipe132005 <soporte@lempsoft.com>
2024-07-24 16:11:52 -0500

    creacion de pagina /administracio/empresas

--- src/pages/Administracion/index.tsx
+++ src/pages/Administracion/index.tsx
@@ -8,9 +8,13 @@ const AdministracionPage = () => {
       <main>
         <ContainerLayout>
           <nav className="grid grid-cols-4 gap-4 mt-6">
-            <Link to={'/administracion/terceros'} className="flex hover:shadow-2xl hover:shadow-slate-950 items-center p-2">
+            <Link to={'/administracion/terceros'} className="flex shadow shadow-slate-500 hover:shadow-2xl hover:shadow-slate-950 items-center p-2">
               <img src="/imgs/Letra_TN.png" alt="letra T" className="w-12 ml-2 aspect-square" />
               <span className="text-lg font-semibold">Terceros</span>
+            </Link>
+            <Link to={'/administracion/empresas'} className="flex shadow shadow-slate-500 hover:shadow-2xl hover:shadow-slate-950 items-center p-2">
+              <img src="/imgs/Letra_EN.png" alt="letra N" className="w-12 ml-2 aspect-square" />
+              <span className="text-lg font-semibold">Empresas</span>
             </Link>
           </nav>
         </ContainerLayout>
