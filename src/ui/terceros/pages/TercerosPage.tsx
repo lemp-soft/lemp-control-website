4f6c20636060f8d4d6b98909516db7e2c57fe844
felipe132005 <soporte@lempsoft.com>
2024-07-30 11:15:16 -0500

    UI/Terceros - creacion de pagina de terceros

---
+++ src/ui/terceros/pages/TercerosPage.tsx
@@ -0,0 +1,16 @@
+import ContainerLayout from "../../../layouts/ContainerLayout"
+import MainLayout from "../../../layouts/MainLayout"
+
+const TercerosPage = () => {
+    return (
+        <MainLayout>
+            <main>
+                <ContainerLayout>
+                    <h1 className="text-2xl font-semibold mt-4">Administracion terceros</h1>
+                </ContainerLayout>
+            </main>
+        </MainLayout>
+    )
+}
+
+export default TercerosPage
