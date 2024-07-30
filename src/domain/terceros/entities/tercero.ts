85a6a0961e6a991d16e81e293116e29042323a73
felipe132005 <soporte@lempsoft.com>
2024-07-30 09:45:49 -0500

    domain - entiti create terceros

---
+++ src/domain/terceros/entities/tercero.ts
@@ -0,0 +1,30 @@
+export type regimenes = "48 - Responsable del impuesto sobre las ventas - IVA" | "49 - no responsable de IVA";
+export type responsabilidades = "O-13 Gran Contribuyente" | "O-15 Autorretenedor" | "O-23 Agente de Retención IVA" | "O-47 Regimen Simple de Tributación" | "R-99-PN No aplica, otros";
+export interface terceros {
+    nit : number;
+    id_municipio : number;
+    tipo : number;
+    dv : string;
+    primer_apellido : string;
+    segundo_apellido? : string;
+    primer_nombre : string;
+    segundo_nombre? : string;
+    razon_social : string;
+    direccion : string;
+    telefono : string;
+    telefono_movil? : string;
+    correo : string;
+    error? : string;
+    imagen? : string;
+    clave_acceso? : string;
+    clave_firma? : string;
+    area? : string;
+    cargo? : string;
+    creado_en? : Date;
+    actualizado_en? : Date;
+    eliminado_en? : Date;
+    regimenes? : regimenes;
+    responsabilidades? : responsabilidades;
+    estado : boolean;
+    codigo_ciiu : number;
+}
--- src/pages/Administracion/Empresas/index.tsx
+++ src/pages/Administracion/Empresas/index.tsx
@@ -1,16 +1,16 @@
 import ContainerLayout from "../../../layouts/ContainerLayout"
 import MainLayout from "../../../layouts/MainLayout"
-import MainTable from "../../../components/Table/MainTable"
-import { useEmpresas } from "../../../hooks/useEmpresas"
+// import MainTable from "../../../components/Table/MainTable"
+// import { useEmpresas } from "../../../hooks/useEmpresas"
 const Empresas = () => {
-    const { data, isLoading, isError } = useEmpresas()
+    // const { data, isLoading, isError } = useEmpresas()
     return (
         <MainLayout>
             <main>
                 <ContainerLayout>
                     <h1 className="font-semibold text-2xl mt-6">Empresas</h1>
                     <section className="mt-6">
-                        <MainTable data={data?.data} error={isError} loading={isLoading} name="empresas" />
+                        {/* <MainTable data={data?.data} error={isError} loading={isLoading} name="empresas" /> */}
                     </section>
                 </ContainerLayout>
             </main>
